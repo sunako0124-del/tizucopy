ea4e76d1f61daa8b94e6981abaff64cc08da5c73
nakatosh <92030557+nakatosh@users.noreply.github.com>
2025-10-15 12:53:45 +0900

    Update t.js

--- t.js
+++ t.js
@@ -1,338 +1,254 @@
+
+//グーグルマップを開く
+function gmap() {
+if (document.getElementById("POLNO").value>0){} else {alert('マーカーを選択してから押すと、グーグルマップで現在地からの経路が表示されます。');return;}
+var glat = document.getElementById("LAT").value;
+  var glng = document.getElementById("LNG").value;
+
+window.open("https://www.google.com/maps?q=" + glat + "," + glng);
+}
+
 var db;
-var indexedDB = window.indexedDB || window.mozIndexedDB || window.msIndexedDB;
-    
-    if (indexedDB) {
-        // データベースを削除したい場合はコメントを外します。
-        //indexedDB.deleteDatabase("mydb");
-        var openRequest = indexedDB.open("CSVMAPdb", 1.0);
-        openRequest.onupgradeneeded = function(event) {
-        // データベースのバージョンに変更があった場合(初めての場合もここを通ります。)
+   var indexedDB = window.indexedDB || window.mozIndexedDB || window.msIndexedDB;
+      if (indexedDB) {
+      // データベースを削除したい場合はコメントを外します。
+      //indexedDB.deleteDatabase("mydb");
+      var openRequest = indexedDB.open("mydb", 1.0);
+       openRequest.onupgradeneeded = function(event) {
+// データベースのバージョンに変更があった場合(初めての場合もここを通ります。)
         db = event.target.result;
-        var store = db.createObjectStore("mystore", { keyPath: "mykey"});
-            store.createIndex("myvalueIndex", "myvalue");
-            }                
-            openRequest.onsuccess = function(event) {
-            db = event.target.result;
-        }
-        } else {
-        window.alert("このブラウザではIndexed DataBase API は使えません。");
-        }
+           var store = db.createObjectStore("mystore", { keyPath: "mykey"});
+              store.createIndex("myvalueIndex", "myvalue");
+
+      }
+          openRequest.onsuccess = function(event) {
+
 
 //マーカークリックイベント
-function markerClick(e){ 
+         db = event.target.result;
+        }
+      } else {
+        window.alert("このブラウザではIndexed DataBase API は使えません。");
+              }
+
+function markerClick(e){
   ck();
   //マーカーから値をもらう
-  NO.value = e.sourceTarget.options.customID;            
+  POLNO.value = e.sourceTarget.options.customID;            
   LAT.value = e.latlng.lat;
   LNG.value = e.latlng.lng;
   ima();
   map.setView([e.latlng.lat, e.latlng.lng]);
 
+  //現在地取得
+  GPS();
+  //接地抵抗クリア 
+  setti1.value = "";
+  //接地抵抗に移動
+  document.getElementById('setti1').focus()
   //DBから値をもらう
-  getValue(); 
+  getValue(event); 
 
 }
 
-
- function getValue() {
-  var key =  Number(document.getElementById("NO").value);   
+// 接地抵抗が入力済みなら取得する
+ function getValue(event) {
+  var key = document.getElementById("POLNO").value;
+  //var result = document.getElementById("result");             
   var transaction = db.transaction(["mystore"], "readwrite");
   var store = transaction.objectStore("mystore");                  
   var request = store.get(key);
+  var element = document.getElementById( "tutibox" ) ;
+  var radioNodeList = element.tuti ;
 
-request.onsuccess = function (event) {
-  var result = event.target.result;
-  if (result) {
-    document.getElementById("GCD").value   = result.mv_1 || "";
-    document.getElementById("biko").value  = result.mv_4 || "";
-    document.getElementById("kflg").value  = result.mv_5 || "";
-    document.getElementById("rank").value  = result.mv_6 || "";
-    document.getElementById("setub").value = result.mv_7 || "";
-    document.getElementById("suryo").value = result.mv_8 || "";
-    document.getElementById("noww").value  = result.mv_10 || "";
-  } else {
-    console.log("データが見つかりませんでした");
+  request.onsuccess = function (event) {  
+
+    if (event.target.result === undefined) {} else {
+      //値あり
+      var aaa = Number(event.target.result.myvalue)
+      //0だったらヌル数字だったら数字
+      if(aaa===0){setti1.value ="";} else {
+      setti1.value = aaa;
+      biko.value = event.target.result.mybiko
+      etc.value = event.target.result.myetc
+      IV.value = event.target.result.myIV
+      BRK.value = event.target.result.myBRK
+      TIK2.value = event.target.result.myTIK2
+      TIK3.value = event.target.result.myTIK3
+      SUTE.value = event.target.result.mySUTE
+      BSY.value = event.target.result.myBSY
+      radioNodeList[event.target.result.mytuti].checked = true ;
+      }
+    }
+    Bsyu();  //単独の時B種を表示する
   }
-};
 }
 
-//入力画面ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-//数量ボタンーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-function suu0() {
-  document.getElementById("suryo").value = 0;
-}
-function suuPlus(value) {
-  document.getElementById("suryo").value =document.getElementById("suryo").value +++ value;
-}
-//グーグルマップを開くーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-function gmap() {
-  if (document.getElementById("NO").value>0){} else {alert('マーカーを選択してから押すと、グーグルマップで現在地からの経路が表示されます。');return;}
-  var glat = document.getElementById("LAT").value;
-    var glng = document.getElementById("LNG").value;
-  window.open("https://www.google.com/maps?q=" + glat + "," + glng);
-  }
+//登録  
+function setValue(event) {
+  var key = document.getElementById("POLNO").value;
+  var value = Number(document.getElementById("setti1").value);
+  var LAT = Number(document.getElementById("LAT").value);
+  var LNG = Number(document.getElementById("LNG").value);
 
+  var GLAT = Number(document.getElementById("GLAT").value);
+  var GLNG = Number(document.getElementById("GLNG").value);
 
+  var now = document.getElementById("noww").value;
+  var etc = document.getElementById("etc").value;
+  var BSY = Number(document.getElementById("BSY").value);
+  var IV = document.getElementById("IV").value;
+  var BRK = document.getElementById("BRK").value;
+  var TIK2 = document.getElementById("TIK2").value;
+  var TIK3 = document.getElementById("TIK3").value;
+  var SUTE = document.getElementById("SUTE").value;
 
-//登録  ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-function setValue() {
-  const g = id => document.getElementById(id), 
-  key = +g("NO").value;
-  if (key <= 0) return alert('マーカーをクリックしてから登録してください!!');
-  const data = {
-    mykey: key, 
-    mv_1: +g("GCD").value,
-    mv_2: +g("LAT").value, 
-    mv_3: +g("LNG").value,
-    mv_4: g("biko").value, 
-    mv_5: 1,
-    mv_6: g("rank").value,  
-    mv_7: +g("setub").value, 
-    mv_8: +g("suryo").value,
-    mv_9: 0,
-    mv_10: g("noww").value
-  };
- 
-  db.transaction(["mystore"], "readwrite").objectStore("mystore").put(data).onsuccess = () =>
-    console.log("保存成功:", key);
+  //チェック
+  if (key >0){} else {alert('マーカーをクリックしてから登録してください!!');return;}
+  if (value == 0){
+    alert('接地抵抗が未入力です');document.getElementById('setti1').focus();return;
+  } 
+  if (etc >= 4 && BSY == 0 ) {
+    alert('単独接地の時は、A種とB種必須入力です。単独だけど1極しかない時は、単独接地を選択せずにそのまま入力してください。');document.getElementById('BSY').focus();return;
+  } 
+   
+  // form要素を取得 
+  var element = document.getElementById( "tutibox" ) ;
+  // form要素内のラジオボタングループ(name="tuti")を取得
+  var radioNodeList = element.tuti ;
+  // 選択状態の値(value)を取得 (Bが選択状態なら"b"が返る)
+  var tuti = radioNodeList.value ;
+  var biko = document.getElementById("biko").value;
+  //                  
+  var transaction = db.transaction(["mystore"], "readwrite");
+  var store = transaction.objectStore("mystore")
+  var request = store.put({ mykey: key, myvalue: value, myBSY: BSY, myLAT: LAT, myLNG: LNG, mytuti: tuti, mybiko: biko, myGLAT: GLAT, myGLNG: GLNG, mynow: now ,myetc:etc, myIV:IV, myBRK:BRK, myTIK2:TIK2, myTIK3:TIK3, mySUTE:SUTE});
+  
+  
+  MAK();
 
-	MAK(1);		
-	resetInputs(); 	 // 入力欄リセット（外部関数呼び出し）
- 	ck0();	//再マーク
+  //入力欄リセット
+	document.getElementById("POLNO").value = "";
+	document.getElementById("setti1").value = "";
+	document.getElementById("biko").value = "";
 
-  // 保存成功時の処理（必要なら追加）
-  request.onsuccess = function () {
-    console.log("データの保存に成功しました:", key);
-  };
-}
+  radioNodeList[0].checked = true ;
 
-//キャンセルーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-function notValue() {
-   const g = id => document.getElementById(id), key = +g("NO").value;
-  if (key <= 0) return alert('マーカーをクリックしてから登録してください!!');
-  const data = {
-    mykey: key, 
-    mv_1: +g("GCD").value,
-    mv_2: +g("LAT").value, 
-    mv_3: +g("LNG").value,
-    mv_4: g("biko").value, 
-    mv_5: +g("kflg").value,
-    mv_6: g("rank").value,  
-    mv_7: +g("setub").value, 
-    mv_8: +g("suryo").value,
-    mv_9: 0,
-    mv_10: g("noww").value
+	document.getElementById("etc").selectedIndex = 0;
+  document.getElementById("BSY").value = "";
+  document.getElementById("IV").value = "";
+  document.getElementById("BRK").value = "";
+  document.getElementById("TIK2").value = "";
+  document.getElementById("TIK3").value = "";
+  document.getElementById("SUTE").value = ""; 
+  
+  ck0();
+  currentWatchReset();
+   request.onsuccess = function (event) {
 
-  };
- //db登録
-  db.transaction(["mystore"], "readwrite").objectStore("mystore").put(data).onsuccess = () =>
-    console.log("保存成功:", key);
-
-  MAK(data.mv_5); //マーカ一の色一個更新
-  resetInputs();  // 入力欄リセット（外部関数呼び出し）
-  ck0();//再マーク
-
-  // 保存成功時の処理（必要なら追加）
-  request.onsuccess = function () {
-    console.log("データの保存に成功しました:", key);
-  };
+   }
 }
 
-// 入力欄をリセットする関数ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
-function resetInputs() {
-  const ids = ["NO", "GCD", "kflg", "biko", "rank", "setub", "suryo", "LAT", "LNG", "noww"];
-  ids.forEach(id => {
-    const el = document.getElementById(id);
-    if (el) el.value = "";
-  });
-}
-
-
-
-// LDBからマーカーーーーーーーーーーーーーーーーーーーーーーーー
-function MAK(flg) {
-var key = document.getElementById("NO").value;
-var LAT = Number(document.getElementById("LAT").value);
-var LNG = Number(document.getElementById("LNG").value);
-// 既存マーカーの削除
-KAN.eachLayer((layer)=> {if (key === layer.options.customID) {KAN.removeLayer(layer);}});
-moji.eachLayer((layer)=> {if (key === layer.options.customID) {moji.removeLayer(layer);}});
-ho.eachLayer((layer)=> {if (key === layer.options.customID) {ho.removeLayer(layer);}});
-MI.eachLayer((layer)=> {if (key === layer.options.customID) {MI.removeLayer(layer);}});
-//新しいマーカーの追加
-switch (flg) {
-case 1:createMarker(KAN,LAT,LNG,'#fb1bceff',key);break;
-case 0:createMarker(MI,LAT,LNG,'#fdfdfd',key);break;
-case 3:createMarker(ho,LAT, LNG,'#047104ff',key);break;
-case 4:createMarker(ho,LAT, LNG,'#14a9ceff',key);break;
-default:createMarker(MI,LAT, LNG,'#fdfdfd',key);break;
-}          
-
-}
-
-// サークルマーカーを書くーーーーーーーーーーーーーーーーーーーーーーーー
-function createMarker(layer, lat, lng, color, key) {
-  layer.addLayer(
-    L.circleMarker([lat, lng], {
-      color:'#7a797aff', weight: 2, fillColor: color, fillOpacity: 1, radius: 10, customID: key
-    }).on('click', function(e) { markerClick(e); })
-  );
-}
-
-// LDBからマーカーーーーーーーーーーーーーーーーーーーーーーーー
-function MAKall() {
+// LDBからマーカ
+function MAKall(event) {
   return new Promise(function(resolve) {
+    var result = document.getElementById("result");                   
     var transaction = db.transaction(["mystore"], "readwrite");
     var store = transaction.objectStore("mystore");
     var request = store.openCursor();
 
-    KAN.clearLayers();
-    ho.clearLayers();
+
+    //マーカーレイヤー削除
     MI.clearLayers();
+    KAN.clearLayers();
+    moji.clearLayers();
 
     request.onsuccess = function (event) {
+      //リストがなかったら終了  
       if(event.target.result == null) {
-        resolve();
-        return;
+      resolve()   
+      return;
       }
-
       var cursor = event.target.result;
       var data = cursor.value;
+      var divIcon3 = L.divIcon({
+        html: data.mykey.slice( -4 ),
+        className: 'divicon2',
+        iconSize: [0,0],
+      
+        iconAnchor: [0,28]
+      });
+  
+      moji.addLayer(L.marker([data.myLAT, data.myLNG], {icon: divIcon3,customID: data.mykey}).on('click', function(e) { markerClick(e);}));
 
-  var divIcon3 = L.divIcon({html: String(data.mykey).slice(-4),  className: 'divicon2',  iconSize: [0,0],  iconAnchor: [-15,15]});
+      //値が入ってたら完了（グレー）、未入力ピンク
+      if(data.myvalue>0){
+        
+      //L.marker([data.myLAT, data.myLNG],{icon:myIcon2,customID: data.mykey}).addTo(KAN).on('click', function(e) { markerClick(e);});
+      KAN.addLayer(
+            L.circleMarker([data.myLAT, data.myLNG],{
+          color: '#fdfdfd',
+          weight: 1,
+          opacity: 1,
+          fillColor: '#534f4f',
+          fillOpacity: 1,
+          radius: 10,
+          
+          customID: data.mykey })
 
-//ステータスで色を変えたい
-switch (data.mv_5) {
-	case 1: addMarkerToLayer(KAN,data,'#fb1bceff',divIcon3);break;
-	case 0: addMarkerToLayer(MI,data,'#fdfdfd',divIcon3);break;
-	case 3: addMarkerToLayer(ho,data,'#047104ff',divIcon3);break;
-	case 4: addMarkerToLayer(ho,data,'#14a9ceff',divIcon3);break;
-	default: addMarkerToLayer(MI,data,'#fdfdfd',divIcon3);
-}   
-	cursor.continue();
-};
-});
-}
+        .on('click', function(e) { markerClick(e);})
+      );
+      } else {
+        
+        MI.addLayer(
+          L.circleMarker([data.myLAT, data.myLNG],{
+            color: '#fdfdfd',
+            weight: 1,
+            opacity: 1,
+            fillColor: '#fa04b0',
+            fillOpacity: 1,
+            radius: 10,
+            
+          customID: data.mykey})
 
-//ステータスで色を変えたいーーーーーーーーーーーーーーーーーーーーーーーー
-function addMarkerToLayer(layer, data, color, divIcon3) {
-const lat = parseFloat(data.mv_2);
-const lng = parseFloat(data.mv_3);
-const kno =  +document.getElementById("PullDownList").value;
-const bounds = map.getBounds(); // 現在の地図範囲
-// 通常のマーカーは kno が null または一致する場合のみ追加
-if (!kno || kno === data.mv_1) {layer.addLayer(
-   	  		L.circleMarker([lat, lng], {
-			color: '#7a797aff', weight: 2, fillColor: color,  fillOpacity: 1,  radius: 10, customID: data.mykey	}).on('click', function (e) {markerClick(e);
-    	 		}
-		)
-	);
-	}
-
-}
-
-// LDBからマーカの文字だけーーーーー
-function MAK_text() {
-  return new Promise(function(resolve) {
-    // IndexedDB から "mystore" ストアを読み込み
-    const transaction = db.transaction(["mystore"], "readwrite");
-    const store = transaction.objectStore("mystore");
-    const request = store.openCursor();
-
-    // 既存マーカーの customID を記録するためのセットを作成
-    const existingIDs = new Set();
-
-    // moji レイヤーにすでに追加されているマーカーの customID を収集
-    // この処理はマーカーを消さずに、重複を防ぐために必要
-    moji.eachLayer(layer => {
-      if (layer.options && layer.options.customID) {
-        existingIDs.add(layer.options.customID);
-      }
-    });
-
-    // 地図の現在の表示範囲とズームレベルを取得
-    const bounds = map.getBounds();
-    const kno = +document.getElementById("PullDownList").value;
-
-    // IndexedDB のカーソル処理開始
-    request.onsuccess = function(event) {
-      const cursor = event.target.result;
-
-      // データがもうない場合は Promise を解決して終了
-      if (!cursor) {
-        resolve();
-        return;
+        .on('click', function(e) { markerClick(e);})
+        );
       }
 
-      const data = cursor.value;
-      const lat = parseFloat(data.mv_2); // 緯度
-      const lng = parseFloat(data.mv_3); // 経度
-
-      // ズームが17より大きく、地図範囲内にある場合のみ処理
-      if (map.getZoom() > 17 && bounds.contains([lat, lng])) {
-
-        // PullDownList の選択値と一致するか、未選択なら表示対象
-        // かつ、すでに表示されていない customID の場合のみ追加
-        if ((!kno || kno === data.mv_1) && !existingIDs.has(data.mykey)) {
-
-          // 表示するテキストアイコンを作成（mykey の末尾4文字を表示）
-          const divIcon3 = L.divIcon({
-            html: String(data.mykey).slice(-4),
-            className: 'divicon2',
-            iconSize: [0, 0],
-            iconAnchor: [-15, 15]
-          });
-
-          // マーカーを作成し、customID を設定
-          const marker = L.marker([lat, lng], {
-            icon: divIcon3,
-            customID: data.mykey
-          });
-
-          // moji レイヤーにマーカーを追加（既存と重複しない）
-          moji.addLayer(marker);
-        }
-      }
-
-      // 次のデータへ進む
-      cursor.continue();
-    };
-  });
-}
-
-
-// LDBから線を引く
-function addline() {
-return new Promise(function(resolve) {              
-  var transaction = db.transaction(["mystore"], "readwrite");
-  var store = transaction.objectStore("mystore");
-  var request = store.openCursor();
-  //lineレイヤー削除
-  line.clearLayers();
-  request.onsuccess = function (event) {
-    //リストがなかったら終了  
-    if(event.target.result == null) {
-    resolve()   
-    return;
-     }
-    var cursor = event.target.result;
-    var data = cursor.value;
-
-    if(data.mv_1>0){
-   line.addLayer(
-       L.polyline([[data.mv_2, data.mv_3]], {color: 'red'})
-    );
-    } else {
+     cursor.continue();
     }
-    cursor.continue();
+  })
+}
+
+// LDBからマーカ
+function MAK() {
+  var key = document.getElementById("POLNO").value;
+  var value = Number(document.getElementById("setti1").value);
+  var LAT = Number(document.getElementById("LAT").value);
+  var LNG = Number(document.getElementById("LNG").value);
+
+KAN.eachLayer((layer)=> {if (key === layer.options.customID) {KAN.removeLayer(layer);}});
+MI.eachLayer((layer)=> {if (key === layer.options.customID) {MI.removeLayer(layer);}});
+
+  if (value >0) {
+    KAN.addLayer(L.circleMarker([LAT, LNG],{
+    color: '#fdfdfd',weight: 2,opacity: 1,fillColor: '#534f4f',fillOpacity: 1,radius: 10,customID: key })
+    .on('click', function(e) { markerClick(e);})
+    );
+  }else{
+    MI.addLayer(L.circleMarker([LAT, LNG],{
+    color: '#fdfdfd',weight: 2,opacity: 1,fillColor: '#fa04b0',fillOpacity: 1,radius: 10,customID: key})
+    .on('click', function(e) { markerClick(e);})
+    );
   }
-})
 }
 
 //マーカーが全部入るイメージ
 function zenb(){
-map.fitBounds(ho.getBounds());   
+map.fitBounds(MI.getBounds());   
 };
+
+
 
 function ck(){
 let element = document.getElementById('pop-up');
@@ -348,12 +264,10 @@ function GPS() {
 	function success(pos) {
 		GLAT.value = pos.coords.latitude;
 		GLNG.value = pos.coords.longitude;
-	}
 
-  function error() {
+	}
+  function error(err) {
 		alert('位置情報を取得できませんでした。');
-		GLAT.value = 0;
-		GLNG.value = 0;
 	}
 	var options = {
 		enableHighAccuracy: true,
@@ -401,14 +315,15 @@ function currentWatchReset() {
 	}
 }
 
+
 //待つタイプ
 async function mikan(){
-  await MAKall();
-  await map.fitBounds(ho.getBounds());   
+await MAKall();
+await map.fitBounds(MI.getBounds());   
 };
 async function kanryo(){
-    await MAKall();
-    await map.fitBounds(KAN.getBounds());   
+await MAKall();
+await map.fitBounds(KAN.getBounds());   
 };
 	
 //現在時刻
@@ -423,11 +338,34 @@ function ima() {
   noww.value =  year + "/" + month + "/" + date + " " + hour + ":" + minute + ":" + second;
 }
 
-//フィーダを選択
-async function inputChange() {
-  await MAKall();
-  if (ho.getLayers().length > 0) {
-    map.fitBounds(ho.getBounds());
-  }
-}
+function Bsyu(){
+const etc = document.getElementById("etc").value
+const st = document.getElementById("setti1").value
+if (etc == '7'){
+document.getElementById('BSY').style.visibility = 'visible';
+document.getElementById('asyu').style.visibility = 'visible';
+document.getElementById('bsyu').style.visibility = 'visible';
+document.getElementById('asyu').value = 'D種';
+document.getElementById('bsyu').value = 'A種';
+    }else{
+if (etc >= '4'){
+document.getElementById('BSY').style.visibility = 'visible';
+document.getElementById('asyu').style.visibility = 'visible';
+document.getElementById('bsyu').style.visibility = 'visible';
+document.getElementById('asyu').value = 'A種';
+document.getElementById('bsyu').value = 'B種';
+    }else{
+    document.getElementById('BSY').style.visibility = 'hidden';
+document.getElementById('asyu').value = '';
+document.getElementById('bsyu').value = '';
+    document.getElementById('asyu').style.visibility = 'hidden';
+    document.getElementById('bsyu').style.visibility = 'hidden';
+  }}
 
+if (etc === '1' && st !== '999'){
+  document.getElementById('setti1').value = '999';
+  }
+  if (etc != '1' && st == "999"){
+    document.getElementById('setti1').value = null;
+    }
+}
